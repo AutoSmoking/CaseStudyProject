01a8ab42684f17e10f8c3f65631c09900824f10f
Teron0214 <kakusansei640@gmail.com>
2020-03-15 15:51:40 +0900

    泡の挙動を作成

---
+++ Assets/Script/BubbleOperation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleOperation : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField, Header("泡の浮上速度"), Range(0, 0.1f)]
+    float FloatAcceleration;
+
+    void Start()
+        {
+        
+        }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ワールドの軸に合わせて移動
+        transform.Translate(new Vector3(0, FloatAcceleration, 0), Space.World);
+
+    }
+}
