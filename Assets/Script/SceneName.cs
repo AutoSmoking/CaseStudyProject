7bb3cd656ea09b51d74fadaf1806a7ec1644f316
Asuka1905 <asu.aka.051612>
2020-04-09 12:59:50 +0900

    StageSelect シーン移動のみ

---
+++ Assets/Script/SceneName.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneName : MonoBehaviour
+{
+    public List<string> Nama = new List<string>();
+    public string TitleName;
+    public SceneComponent SceneScript;
+    public string NowSceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneScript = gameObject.GetComponent<SceneComponent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneScript.SceneName != NowSceneName)
+        {
+            NowSceneName = SceneScript.SceneName;
+        }
+    }
+}
---
+++ Assets/Script/SelectMenu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SelectMenu : MonoBehaviour
+{
+    [SerializeField, Header("1～10のステージシーン名")]
+    public string[] scenenames = new string[10];
+    Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GameObject.Find("Canvas/1~10").GetComponent<Button>();
+
+        //ボタンが選択された状態になる
+
+        button.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            string name = gameObject.name;
+            Debug.Log(int.Parse(name));
+            //SceneComponent instance = GameObject.Find("SceneManager").GetComponent<SceneComponent>();
+            //EventSystem eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+
+            //if (eventSystem.currentSelectedGameObject.gameObject == gameObject)
+            //{
+            //    OnClick(int.Parse(gameObject.name) + 1);
+            //}
+
+        }
+    }
+
+    public void OnClick(int name)
+    {
+        SceneComponent instance = GameObject.Find("SceneManager").GetComponent<SceneComponent>();
+        instance.SceneName = scenenames[name];
+        instance.SceneFlag = true;
+        //for(int i = 0; i < scenename1_10.Length; i++)
+        //{
+        //    if (scenename1_10[i] == i.ToString())
+        //    {
+        //        Debug.Log(scenename1_10[i]);
+        //    }
+        //}
+    }
+
+    public void MoveSelect(int num)
+    {
+        foreach(Transform trans in gameObject.transform)
+        {
+            var go = transform.gameObject;
+        }
+        
+        switch (num)
+        {
+            case 0:
+
+                break;
+
+        }
+    }
+}
