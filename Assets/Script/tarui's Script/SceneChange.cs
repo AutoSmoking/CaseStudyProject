76065664c811ce0231346bae3c0256cc001c3b99
TaruK981221 <tarui2412git@gmail.com>
2020-04-20 08:23:43 +0900

    ステージ切り替えプレハブを作成

---
+++ Assets/Script/tarui's Script/SceneChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    [SerializeField, Header("シーン切り替えボタン")]
+    KeyCode key = KeyCode.Alpha1;
+
+    [SerializeField, Header("切り替え先シーン名(ここに名前を書いてね)")]
+    string scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (scene == null)
+        {
+            Debug.LogError("シーンが設定されていません。");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(key))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
