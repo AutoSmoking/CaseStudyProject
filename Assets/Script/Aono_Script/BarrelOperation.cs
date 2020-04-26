9d3ec3fbd27f27d3ac3f145af5a7b127d3160550
Teron0214 <kakusansei640@gmail.com>
2020-04-26 16:43:11 +0900

    樽の作成/樽のスクリプト（仮）作成/樽をステージに配置

---
+++ Assets/Script/Aono_Script/BarrelOperation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelOperation : MonoBehaviour
+{
+    bool Flag = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Flag = true;
+        }
+
+        if (Flag != false)
+        {
+            //Transform myTransform = this.transform;
+            //Vector3 pos = myTransform.position;
+            //GameObject obj = (GameObject)Resources.Load("Bubble");
+            //Instantiate(obj, new Vector3(pos.x, pos.y, pos.z), Quaternion.identity);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
