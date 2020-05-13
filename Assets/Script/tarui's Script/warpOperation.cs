249bd1556de937a93ef35a114cc6f34c4cbe97c5
TaruK981221 <tarui2412git@gmail.com>
2020-05-14 02:01:20 +0900

    ワープ完成

--- Assets/Script/tarui's Script/warpOperation.cs
+++ Assets/Script/tarui's Script/warpOperation.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class warpOperation : MonoBehaviour
 {
     [SerializeField, Header("ワープ先")]
-    GameObject next;
+    public GameObject next;
 
     [SerializeField, Header("ワープまでの待ち時間"),Range(0.0f,5.0f)]
     float WaitTime;
@@ -19,12 +19,17 @@ public class warpOperation : MonoBehaviour
     [SerializeField, Header("ワープ中に止める対象のオブジェクト")]
     List<GameObject> stopList = new List<GameObject>() { };
 
+    [SerializeField, Header("ゴールかどうか")]
+    bool GoalFlg = false;
+
     // ワープ中の待ち時間用
     bool WaitFlg = false;
     // ワープ中を表すフラグ
     bool WarpFlg = false;
 
-    GameObject Object;
+    // ワープしているオブジェクトを一時格納
+    [System.NonSerialized]
+    public GameObject Object;
 
     Vector3 firstPos;
 
@@ -37,19 +42,29 @@ public class warpOperation : MonoBehaviour
         {
             Debug.LogError("ワープする対象がないです。");
         }
+
+        if(next == null)
+        {
+            Debug.LogError("ワープ先がないです。");
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        // ワープ開始
         if(WarpFlg)
         {
+            // ワープ中は待機
             if (WaitFlg)
             {
+                // 待ち時間が終わったらワープ完了
                 if(percent >= WarpTime)
                 {
                     percent = 0;
                     WarpFlg = false;
+                    // 自身のオブジェクトをワープの出口に切り替える
+                    GoalFlg = true;
 
                     // ワープが終わったらオブジェクトを出現させる
                     Object.SetActive(true);
@@ -68,16 +83,28 @@ public class warpOperation : MonoBehaviour
                         }
                     }
                 }
+                // ワープしている間は待つ
                 else
                 {
                     percent += Time.deltaTime;
                 }
             }
+            // ワープするまではゆっくりとワープオブジェクトの
+            // 中心に移動
             else
             {
+                // 中心に移動しきったらワープさせる
                 if (percent >= WaitTime)
                 {
+                    // 対象オブジェクトをワープ先まで移動
                     Object.transform.position = next.transform.position;
+
+                    // ワープ先の次のワープ先として自分のオブジェクトを設定する
+                    next.GetComponent<warpOperation>().next = this.gameObject;
+
+                    // ワープ先に現在ワープしているオブジェクトを設定
+                    next.GetComponent<warpOperation>().Object = Object;
+
                     percent = 0;
                     //WarpFlg = false;
                     WaitFlg = true;
@@ -85,6 +112,7 @@ public class warpOperation : MonoBehaviour
                     // ワープ中はオブジェクトは消す
                     Object.SetActive(false);
                 }
+                // ワープ対象のオブジェクトが自身の中心まで移動
                 else
                 {
                     percent += Time.deltaTime;
@@ -97,34 +125,58 @@ public class warpOperation : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        foreach(var obj in objList)
+        // 自身がワープの入り口かつワープ中でないなら処理
+        if(!GoalFlg && !WarpFlg)
         {
-            if(obj == other.gameObject && WarpFlg == false)
+            foreach (var obj in objList)
             {
-                //obj.transform.position = next.transform.position;
-                WarpFlg = true;
-
-                Object = obj;
-
-                firstPos = obj.transform.position;
-
-                foreach (var stopObj in stopList)
+                if (obj == other.gameObject)
                 {
-                    foreach (var com in stopObj.GetComponents<MonoBehaviour>())
+                    // ワープを開始するフラグ
+                    WarpFlg = true;
+
+                    // ワープの対象となるオブジェクトを格納
+                    Object = obj;
+
+                    // 位置の補間をするためにこの瞬間の位置を格納
+                    firstPos = obj.transform.position;
+
+                    // ワープしている間は位置が変わるオブジェクトや
+                    // 操作するオブジェクトは停止する
+                    foreach (var stopObj in stopList)
                     {
-                        com.enabled = false;
+                        foreach (var com in stopObj.GetComponents<MonoBehaviour>())
+                        {
+                            com.enabled = false;
+                        }
+
+                        if (stopObj.GetComponent<Rigidbody>() != null)
+                        {
+                            stopObj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                            stopObj.GetComponent<Rigidbody>().isKinematic = true;
+                        }
                     }
 
-                    if(stopObj.GetComponent<Rigidbody>() != null)
-                    {
-                        stopObj.GetComponent<Rigidbody>().velocity = Vector3.zero;
-                        stopObj.GetComponent<Rigidbody>().isKinematic = true;
-                    }
+                    break;
                 }
+            }
+        }
+    }
 
-                break;
+    private void OnTriggerExit(Collider other)
+    {
+        // 自身がワープの出口かつワープ対象のオブジェクトが格納されているとき
+        // 判定から外れた瞬間、ワープの入り口にする
+        if(GoalFlg)
+        {
+            if(Object == other.gameObject)
+            {
+                // 初期化
+                Object = null;
+
+                GoalFlg = false;
             }
         }
     }
