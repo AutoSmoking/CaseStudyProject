bd930a45d3e1baad62eee747524b57d483e5abbd
TaruK981221 <tarui2412git@gmail.com>
2020-05-18 11:25:39 +0900

    宝箱のアニメーションを修正 それに伴うシーンの調整 その他修正

--- Assets/Script/Aono_Script/GoalHitOperation.cs
+++ Assets/Script/Aono_Script/GoalHitOperation.cs
@@ -4,24 +4,44 @@ using UnityEngine;
 
 public class GoalHitOperation : MonoBehaviour
 {
+    Animator ani;
+
+    GameObject bubble1 = null;
+    GameObject bubble2 = null;
+    GameObject bubble3 = null;
+
+    bool OpenFlg = false;
+
     // Start is called before the first frame update
 
     void Start()
     {
+        ani = this.GetComponentInChildren<Animator>();
 
+        ani.enabled = false;
+
+        bubble1 = GameObject.FindGameObjectWithTag("1");
+        bubble2 = GameObject.FindGameObjectWithTag("2");
+        bubble3 = GameObject.FindGameObjectWithTag("3");
     }
 
-    void OnTriggerEnter(Collider other)
-    {   //ゴールに接触した時にログを出す
-        if (other.gameObject.tag == "Player")
-        {
-            Debug.Log("GoalHit");
-        }
-    }
+    //void OnTriggerEnter(Collider other)
+    //{   //ゴールに接触した時にログを出す
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        Debug.Log("GoalHit");
+    //    }
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!OpenFlg && bubble1 != null && !bubble1.GetComponent<BubbleOperation>().DeathFlg
+            && bubble2 == null && bubble3 == null)
+        {
+            OpenFlg = true;
+
+            ani.enabled = true;
+        }
     }
 }
--- Assets/Script/tarui's Script/jikken.cs
+++ Assets/Script/tarui's Script/jikken.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class jikken : MonoBehaviour
 {
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        anim = this.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.Play(0);
+        }
     }
 }
