c3a76ce090b3aaf634a9c321c1d0c0cda9c93e37
Asuka1905 <asu.aka.051612>
2020-06-04 21:09:00 +0900

    コントローラこれでどう？ フェードはまだ出来てない

--- Assets/Script/tanaka/FadeOut.cs
+++ Assets/Script/tanaka/FadeOut.cs
@@ -7,21 +7,42 @@ public class FadeOut : MonoBehaviour
 {
     [SerializeField, Header("フェードアウト処理したいので追加")]
     public Image Panel;
-    public bool FadeTrg;
+    public bool FadeTrgIn;
+    public bool FadeTrgOut;
     // Start is called before the first frame update
     void Start()
     {
-        FadeTrg = false;
+        FadeTrgIn = false;
+        FadeTrgOut = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Panel.color -= new Color(0.0f, 0.0f, 0.0f, 0.005f);
-
-        if (Panel.color.a <= 0)
+        if (!FadeTrgIn)
         {
-            FadeTrg = true;
+            Panel.color += new Color(0.0f, 0.0f, 0.0f, 0.005f);
         }
+
+        if (Panel.color.a >= 1.0f) 
+        {
+            FadeTrgIn = true;
+        }
+        if (!FadeTrgOut)
+        {
+            Panel.color -= new Color(0.0f, 0.0f, 0.0f, 0.005f);
+        }
+
+        if (Panel.color.a <= 0.0f) 
+        {
+            FadeTrgOut = true;
+        }
+    }
+
+    public void ReSetFade()
+    {
+        Panel.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+        FadeTrgIn = false;
+        FadeTrgOut = false;
     }
 }
