3c28b81f1d541a4f345b87310ec0aa2496173d4d
TaruK981221 <tarui2412git@gmail.com>
2020-07-09 18:59:42 +0900

    チョウチンアンコウのモデル追加 ステージ改変 アンコウのプレハブ作成

---
+++ Assets/Script/tarui's Script/LightPos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPos : MonoBehaviour
+{
+    [SerializeField]
+    Transform fish = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.localPosition = fish.localPosition + new Vector3(0, 0, -12.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localPosition = fish.localPosition + new Vector3(0, 0, -12.0f);
+    }
+}
